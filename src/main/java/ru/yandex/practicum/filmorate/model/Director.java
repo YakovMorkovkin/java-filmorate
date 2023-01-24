4eb9aa84380683dc983f3c3e7dab88b79e8c9a70
YakovMorkovkin <yakovmorkovkin@yandex.ru>
2023-01-24 13:22:11 +0300

    add director functional(small corrections -1st rev)

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Data
 public class Director implements Serializable {
     private int id;
+    @NotBlank
     @NotEmpty(message = "Имя режиссера не может быть пустым.")
     private String name;
 
